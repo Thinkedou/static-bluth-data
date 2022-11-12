8bce0b710fbf26670d71cb43ebc805804549aa5e
Thinkedou <r.enguehard@gmail.com>
2022-11-12 19:32:40 +0100

    first

---
+++ js/recipientsGroup.js
@@ -0,0 +1,226 @@
+export const recipientsGroup = [
+  {
+    "title": "Marketing",
+    "icon":"💥",
+    "id":"5f180bca383c08046480682c",
+    "recipients": [
+        {
+          "firstName": "Dusty",
+          "lastName": "Treutel",
+          "email": "Dusty.Treutel@gmail.com",
+          "avatar": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/40.jpg"
+      },
+      {
+          "firstName": "Haskell",
+          "lastName": "Hegmann",
+          "email": "Haskell22@yahoo.com",
+          "avatar": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/701.jpg"
+      },
+      {
+          "firstName": "Geovanni",
+          "lastName": "Homenick",
+          "email": "Geovanni.Homenick@hotmail.com",
+          "avatar": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/463.jpg"
+      },
+      {
+          "firstName": "Velva",
+          "lastName": "Monahan",
+          "email": "Velva_Monahan@yahoo.com",
+          "avatar": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/889.jpg"
+      },
+      {
+          "firstName": "Arlo",
+          "lastName": "Gutmann",
+          "email": "Arlo52@hotmail.com",
+          "avatar": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1066.jpg"
+      }
+    ]
+  },
+  {
+    "title": "Finance",
+    "id":"5f1862ae97d9047915105f2f",
+    "icon":"💲",
+    "recipients": [
+
+        {
+            "firstName": "Jeanette",
+            "lastName": "Aufderhar",
+            "email": "Jeanette_Aufderhar@hotmail.com",
+            "avatar": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/330.jpg"
+        },
+        {
+            "firstName": "Jayson",
+            "lastName": "Reichel",
+            "email": "Jayson_Reichel83@hotmail.com",
+            "avatar": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/730.jpg"
+        },
+        {
+            "firstName": "Claudine",
+            "lastName": "Cole",
+            "email": "Claudine.Cole@gmail.com",
+            "avatar": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/593.jpg"
+        }
+    
+    ]
+  },
+  {
+    "title": "Qualité",
+    "icon":"⏱",
+    "id":"5f1862e197d9047915105f30",
+    "recipients": [
+      {
+          "firstName": "Birdie",
+          "lastName": "Pouros",
+          "email": "Birdie27@gmail.com",
+          "avatar": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/646.jpg"
+      },
+      {
+          "firstName": "Modesto",
+          "lastName": "Becker",
+          "email": "Modesto_Becker@gmail.com",
+          "avatar": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/841.jpg"
+      },
+      {
+          "firstName": "Retha",
+          "lastName": "Leannon",
+          "email": "Retha.Leannon@hotmail.com",
+          "avatar": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/256.jpg"
+      },
+      {
+          "firstName": "Alan",
+          "lastName": "Williamson",
+          "email": "Alan.Williamson@hotmail.com",
+          "avatar": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/974.jpg"
+      }
+    ]
+  },
+  {
+    "title": "Communication",
+    "id":"5f1862e197d9047915105f31",
+    "icon":"🎙",
+    "recipients": [
+      {
+          "firstName": "Francis",
+          "lastName": "Yost",
+          "email": "Francis.Yost@yahoo.com",
+          "avatar": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/289.jpg"
+      },
+      {
+          "firstName": "Forest",
+          "lastName": "Armstrong",
+          "email": "Forest.Armstrong@gmail.com",
+          "avatar": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/406.jpg"
+      }
+    ]
+  },
+  {
+    "title": "R&D",
+    "icon":"🧾",
+    "id":"5f1862e197d9047915105f32",
+    "recipients": [
+      {
+          "firstName": "Lola",
+          "lastName": "Pfannerstill",
+          "email": "Lola.Pfannerstill1@gmail.com",
+          "avatar": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/117.jpg"
+      },
+      {
+          "firstName": "Izaiah",
+          "lastName": "Emard",
+          "email": "Izaiah62@yahoo.com",
+          "avatar": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/351.jpg"
+      },
+      {
+          "firstName": "Fletcher",
+          "lastName": "Maggio",
+          "email": "Fletcher_Maggio70@gmail.com",
+          "avatar": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/675.jpg"
+      },
+      {
+          "firstName": "Viviane",
+          "lastName": "Berge",
+          "email": "Viviane.Berge22@gmail.com",
+          "avatar": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/147.jpg"
+      },
+      {
+          "firstName": "Erna",
+          "lastName": "O'Reilly",
+          "email": "Erna.OReilly@hotmail.com",
+          "avatar": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/185.jpg"
+      },
+      {
+          "firstName": "Rosalind",
+          "lastName": "Breitenberg",
+          "email": "Rosalind_Breitenberg72@hotmail.com",
+          "avatar": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/422.jpg"
+      },
+      {
+          "firstName": "Raegan",
+          "lastName": "Graham",
+          "email": "Raegan_Graham@yahoo.com",
+          "avatar": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/203.jpg"
+      },
+      {
+          "firstName": "Stevie",
+          "lastName": "Emard",
+          "email": "Stevie_Emard@gmail.com",
+          "avatar": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1165.jpg"
+      },
+      {
+          "firstName": "Melba",
+          "lastName": "Okuneva",
+          "email": "Melba20@hotmail.com",
+          "avatar": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/681.jpg"
+      },
+      {
+          "firstName": "Baron",
+          "lastName": "Vandervort",
+          "email": "Baron_Vandervort31@hotmail.com",
+          "avatar": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/102.jpg"
+      },
+      {
+          "firstName": "Kevin",
+          "lastName": "Hessel",
+          "email": "Kevin_Hessel@yahoo.com",
+          "avatar": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/472.jpg"
+      },
+      {
+          "firstName": "Gussie",
+          "lastName": "Cormier",
+          "email": "Gussie.Cormier97@yahoo.com",
+          "avatar": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/130.jpg"
+      }
+  ]
+  },
+  {
+    "title": "Exec",
+    "icon":"📈",
+    "id" :"5f1862e197d9047915105f33",
+    "recipients": [
+      {
+          "firstName": "Ernesto",
+          "lastName": "Prosacco",
+          "email": "Ernesto30@gmail.com",
+          "avatar": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/786.jpg"
+      },
+      {
+          "firstName": "Rosie",
+          "lastName": "Okuneva",
+          "email": "Rosie34@gmail.com",
+          "avatar": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/176.jpg"
+      },
+      {
+          "firstName": "Jeremie",
+          "lastName": "Mante",
+          "email": "Jeremie_Mante@gmail.com",
+          "avatar": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/909.jpg"
+      },
+      {
+          "firstName": "Albin",
+          "lastName": "Lakin",
+          "email": "Albin22@gmail.com",
+          "avatar": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/277.jpg"
+      }
+    ]
+  }
+]
